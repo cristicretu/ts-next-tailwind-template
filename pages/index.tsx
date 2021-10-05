6c0f91dba968e8c318a3cd8f2e6f7a09c7e04b4e
Cristi Cretu <crisemcr@gmail.com>
2021-10-05 17:26:56 +0300

    change text size on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Container>
       <div className="flex flex-col items-center justify-center py-2">
-        <div className="text-xl sm:text-6xl md:text-9xl font-extrabold tracking-tight flex flex-col items-center">
+        <div className="text-6xl md:text-9xl font-extrabold tracking-tight flex flex-col items-center">
           <BigText slides={["NextJs.", "TailwindCSS.", "Typescript."]} />
         </div>
       </div>
