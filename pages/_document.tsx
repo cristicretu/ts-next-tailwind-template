94bce5de4b4cfa52c3c42b132a4c54146c081078
Cristian Crețu <crisemcr@gmail.com>
2022-03-12 16:00:34 +0200

    refactor: _document page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,12 +17,34 @@ class MyDocument extends Document {
             type='font/woff2'
             crossOrigin='anonymous'
           />
-          {/* <link href="/static/favicons/site.webmanifest" rel="manifest" /> */}
           <link href='/static/favicons/favicon.ico' rel='shortcut icon' />
-          {/* <meta
-            content="/static/favicons/browserconfig.xml"
-            name="msapplication-config"
-          /> */}
+          <link href='/static/favicons/site.webmanifest' rel='manifest' />
+          <link
+            href='/static/favicons/apple-touch-icon.png'
+            rel='apple-touch-icon'
+            sizes='180x180'
+          />
+          <link
+            href='/static/favicons/favicon-32x32.png'
+            rel='icon'
+            sizes='32x32'
+            type='image/png'
+          />
+          <link
+            href='/static/favicons/favicon-16x16.png'
+            rel='icon'
+            sizes='16x16'
+            type='image/png'
+          />
+          <link
+            color='#4a9885'
+            href='/static/favicons/safari-pinned-tab.svg'
+            rel='mask-icon'
+          />
+          <meta
+            content='/static/favicons/browserconfig.xml'
+            name='msapplication-config'
+          />
           <meta
             name='theme-color'
             content='#ffffff'
